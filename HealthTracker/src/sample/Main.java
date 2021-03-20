f62bc7abb547ffb30201c58f0d83ce887f65d0f4
Thiruisa <45204969+Thiruisa@users.noreply.github.com>
2021-03-20 20:25:52 +0000

    Add files via upload

--- HealthTracker/src/sample/Main.java
+++ HealthTracker/src/sample/Main.java
@@ -8,16 +8,21 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 import javafx.stage.Window;
 
+
 public class Main extends Application{
     private Stage stage;
     @Override
     public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Health Tracker");
         stage = primaryStage;
         Scene scene = logInScene();
         primaryStage.setScene(scene);
@@ -25,32 +30,30 @@ public class Main extends Application{
     }
 
     private Scene logInScene(){
+
         GridPane gridPaneLogin = new GridPane();
 
         gridPaneLogin.setAlignment(Pos.CENTER);
 
-        // Set a padding of 20px on each side
         gridPaneLogin.setPadding(new Insets(40, 40, 40, 40));
 
-        // Set the horizontal gap between columns
         gridPaneLogin.setHgap(10);
 
-        // Set the vertical gap between rows
         gridPaneLogin.setVgap(10);
 
-        Label headerLabel = new Label("Login Form");
+        Label headerLabel = new Label("User Login");
         headerLabel.setFont(Font.font("Arial", FontWeight.BOLD, 24));
         gridPaneLogin.add(headerLabel, 0,0,2,1);
         GridPane.setHalignment(headerLabel, HPos.CENTER);
-//        headerLabel.setTranslateX(10);
-//        headerLabel.setTranslateY(90);
         GridPane.setMargin(headerLabel, new Insets(20, 0,20,0));
 
 
-        Label userLabel = new Label("Username:");
+        Label userLabel = new Label("Username :");
         gridPaneLogin.add(userLabel, 0,2);
 
         TextField userField = new TextField();
+        userField.setPromptText("Username");
+        userField.setFocusTraversable(false);
         userField.setPrefHeight(40);
         userField.setPrefWidth(300);
         gridPaneLogin.add(userField, 1,2);
@@ -59,6 +62,8 @@ public class Main extends Application{
         gridPaneLogin.add(passwordLabel, 0, 4);
 
         PasswordField passwordField = new PasswordField();
+        passwordField.setPromptText("Password");
+        passwordField.setFocusTraversable(false);
         passwordField.setPrefHeight(40);
         passwordField.setPrefWidth(300);
         gridPaneLogin.add(passwordField, 1, 4);
@@ -67,8 +72,9 @@ public class Main extends Application{
         Button createLoginButton = new Button("Login");
         createLoginButton.setPrefHeight(40);
         createLoginButton.setDefaultButton(true);
-        createLoginButton.setPrefWidth(100);
+        createLoginButton.setPrefWidth(200);
         createLoginButton.setTranslateY(200);
+        createLoginButton.setStyle("-fx-background-color: #3D405B; -fx-text-fill: #F4F1DE; -fx-font-weight: bold;");
         GridPane.setMargin(createLoginButton, new Insets(20, 0,20,0));
 
         createLoginButton.setOnAction(new EventHandler<ActionEvent>() {
@@ -84,15 +90,19 @@ public class Main extends Application{
                 }
 
                 showAlert(Alert.AlertType.CONFIRMATION, gridPaneLogin.getScene().getWindow(), "Login Successful!", "Welcome " + userField.getText());
+
+                System.out.println("User " + userField.getText() + " has logged in");
+
             }
         });
 
         Button createAccountButton = new Button("Create Account");
         createAccountButton.setPrefHeight(40);
         createAccountButton.setDefaultButton(true);
-        createAccountButton.setPrefWidth(100);
+        createAccountButton.setPrefWidth(200);
         createAccountButton.setTranslateX(310);
         createAccountButton.setTranslateY(200);
+        createAccountButton.setStyle("-fx-background-color: #3D405B; -fx-text-fill: #F4F1DE; -fx-font-weight: bold;");
         GridPane.setMargin(createAccountButton, new Insets(20, 0,20,0));
 
         createAccountButton.setOnAction(new EventHandler<ActionEvent>(){
@@ -102,86 +112,79 @@ public class Main extends Application{
         });
         gridPaneLogin.getChildren().addAll(createLoginButton, createAccountButton);
         return new Scene(gridPaneLogin, 1200, 700);
+
     }
 
 
     private Scene CreateAccountScene() {
+
         GridPane gridPaneRegister = new GridPane();
 
         gridPaneRegister.setAlignment(Pos.CENTER);
 
-        // Set a padding of 20px on each side
         gridPaneRegister.setPadding(new Insets(40, 40, 40, 40));
 
-        // Set the horizontal gap between columns
         gridPaneRegister.setHgap(10);
 
-        // Set the vertical gap between rows
         gridPaneRegister.setVgap(10);
+
 
         Label headerLabel = new Label("User Registration");
         headerLabel.setFont(Font.font("Arial", FontWeight.BOLD, 24));
         gridPaneRegister.add(headerLabel, 0,0,2,1);
-        GridPane.setHalignment(headerLabel, HPos.CENTER);
+        headerLabel.setTranslateX(50);
+        headerLabel.setTranslateY(-150);
         GridPane.setMargin(headerLabel, new Insets(20, 0,20,0));
 
-        // Add Name Label
         Label nameLabel = new Label("Full Name : ");
         nameLabel.setTranslateX(-80);
-        nameLabel.setTranslateY(50);
+        nameLabel.setTranslateY(-50);
 
-        // Add Name Text Field
         TextField nameField = new TextField();
+        nameField.setPromptText("Full Name");
+        nameField.setFocusTraversable(false);
         nameField.setPrefHeight(40);
-        nameField.setPrefWidth(300);
-        nameField.setTranslateY(50);
+        nameField.setTranslateY(-50);
+
 
         Label userLabel = new Label("Username :");
         userLabel.setTranslateX(-80);
-        userLabel.setTranslateY(100);
 
         TextField userField = new TextField();
+        userField.setPromptText("Username");
+        userField.setFocusTraversable(false);
         userField.setPrefHeight(40);
-        userField.setPrefWidth(300);
-        userField.setTranslateY(100);
 
-        // Add Email Label
+
         Label emailLabel = new Label("Email : ");
         emailLabel.setTranslateX(-80);
-        emailLabel.setTranslateY(150);
+        emailLabel.setTranslateY(50);
 
-        // Add Email Text Field
+
         TextField emailField = new TextField();
+        emailField.setPromptText("example@hotmail.co.uk");
+        emailField.setFocusTraversable(false);
         emailField.setPrefHeight(40);
-        emailField.setPrefWidth(300);
-        emailField.setTranslateY(150);
+        emailField.setTranslateY(50);
+
 
         Label passwordLabel = new Label("Password : ");
         passwordLabel.setTranslateX(-80);
-        passwordLabel.setTranslateY(200);
+        passwordLabel.setTranslateY(100);
 
         PasswordField passwordField = new PasswordField();
+        passwordField.setPromptText("Password");
+        passwordField.setFocusTraversable(false);
         passwordField.setPrefHeight(40);
-        passwordField.setPrefWidth(300);
-        passwordField.setTranslateY(200);
-
-//        // Confirm Password Label
-//        Label confirmPasswordLabel = new Label("Confirm Password : ");
-//        confirmPasswordLabel.setTranslateX(-80);
-//        confirmPasswordLabel.setTranslateY(250);
-//
-//        // Confirm Password Field
-//        PasswordField confirmPasswordField = new PasswordField();
-//        confirmPasswordField.setPrefHeight(40);
-//        confirmPasswordField.setPrefWidth(300);
-//        confirmPasswordField.setTranslateY(250);
-
+        passwordField.setTranslateY(100);
 
         Button submitButton = new Button("Submit");
         submitButton.setPrefHeight(40);
         submitButton.setDefaultButton(true);
-        submitButton.setPrefWidth(100);
-        submitButton.setTranslateY(300);
+        submitButton.setPrefWidth(300);
+        submitButton.setTranslateX(-50);
+        submitButton.setTranslateY(170);
+        submitButton.setStyle("-fx-background-color: #3D405B; -fx-text-fill: #F4F1DE; -fx-font-weight: bold; ");
         GridPane.setHalignment(submitButton, HPos.CENTER);
         GridPane.setMargin(submitButton, new Insets(20, 0,20,50));
 
@@ -196,30 +199,21 @@ public class Main extends Application{
                     return;
                 }
                 if(emailField.getText().isEmpty()) {
-                    showAlert(Alert.AlertType.ERROR, gridPaneRegister.getScene().getWindow(), "Form Error!", "Please enter your email id");
+                    showAlert(Alert.AlertType.ERROR, gridPaneRegister.getScene().getWindow(), "Form Error!", "Please enter your email address");
                     return;
                 }
                 if(passwordField.getText().isEmpty()) {
                     showAlert(Alert.AlertType.ERROR, gridPaneRegister.getScene().getWindow(), "Form Error!", "Please enter a password");
                     return;
                 }
-//                if(confirmPasswordField.getText().isEmpty()) {
-//                    showAlert(Alert.AlertType.ERROR, gridPaneRegister.getScene().getWindow(), "Form Error!", "Check Password");
-//                    return;
-//                }
 
                 showAlert(Alert.AlertType.CONFIRMATION, gridPaneRegister.getScene().getWindow(), "Registration Successful!", "Welcome " + userField.getText());
 
-                //System.out.println("Account for user " + userField.getText() + " was created successfully");
+                System.out.println("Account for user " + userField.getText() + " was created successfully");
             }
         });
-        gridPaneRegister.getChildren().addAll(
-                nameLabel, nameField,
-                userLabel, userField,
-                emailLabel, emailField,
-                passwordLabel,passwordField,
-                //confirmPasswordLabel, confirmPasswordField,
-                submitButton);
+        gridPaneRegister.getChildren().addAll(nameLabel, nameField, userLabel, userField, emailLabel, emailField,
+                passwordLabel,passwordField, submitButton);
         return new Scene(gridPaneRegister,1200, 700);
 
     }
