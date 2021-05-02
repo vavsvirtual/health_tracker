35031a91ae7b8da4fab25c9813f8d7dab88a51cb
Thiruisa <45204969+Thiruisa@users.noreply.github.com>
2021-05-02 21:39:54 +0100

    Add files via upload

---
+++ Health Tracker 1/Main.java
@@ -0,0 +1,309 @@
+package Frontend;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+import javafx.scene.image.Image;
+
+
+public class Main extends Application{
+    private static Stage stage;
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Health Tracker");
+        stage = primaryStage;
+        Scene scene = logInScene();
+        primaryStage.setScene(scene);
+        primaryStage.sizeToScene();
+        primaryStage.show();
+    }
+
+    public static Scene logInScene(){
+
+        HBox root = new HBox();
+
+        root.setStyle("-fx-background-color: white;");
+
+        String imageUrl = "https://images.unsplash.com/photo-1514995428455-447d4443fa7f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80";
+
+        Image image = new Image(imageUrl,550,700,false,true);
+
+        ImageView imageView = new ImageView(image);
+
+        root.getChildren().add(imageView);
+
+
+        GridPane gridPaneLogin = new GridPane();
+
+        gridPaneLogin.setTranslateX(-310);
+        gridPaneLogin.setTranslateY(200);
+
+        gridPaneLogin.setPadding(new Insets(40, 40, 40, 40));
+
+        gridPaneLogin.setHgap(10);
+        gridPaneLogin.setVgap(10);
+
+
+        Label headerLabel = new Label("User Login");
+        headerLabel.setFont(Font.font("Arial", FontWeight.BOLD, 24));
+        gridPaneLogin.add(headerLabel, 0,0,2,1);
+        headerLabel.setTranslateX(200);
+        GridPane.setHalignment(headerLabel, HPos.CENTER);
+        GridPane.setMargin(headerLabel, new Insets(20, 0,20,0));
+
+
+        Label userLabel = new Label("Username :");
+        userLabel.setTranslateX(-80);
+        gridPaneLogin.add(userLabel, 1,2);
+
+        TextField userField = new TextField();
+        userField.setPromptText("Username");
+        userField.setFocusTraversable(false);
+        userField.setPrefHeight(40);
+        userField.setPrefWidth(400);
+        gridPaneLogin.add(userField, 1,2);
+
+
+        Label passwordLabel = new Label("Password : ");
+        passwordLabel.setTranslateX(-80);
+        gridPaneLogin.add(passwordLabel, 1, 4);
+
+        PasswordField passwordField = new PasswordField();
+        passwordField.setPromptText("Password");
+        passwordField.setFocusTraversable(false);
+        passwordField.setPrefHeight(40);
+        passwordField.setPrefWidth(400);
+        gridPaneLogin.add(passwordField, 1, 4);
+
+
+        Button createLoginButton = new Button("Login");
+        createLoginButton.setPrefHeight(40);
+        createLoginButton.setDefaultButton(true);
+        createLoginButton.setPrefWidth(400);
+        createLoginButton.setTranslateX(410);
+        createLoginButton.setTranslateY(210);
+        createLoginButton.setStyle("-fx-background-color: #3D405B; -fx-text-fill: #F4F1DE; -fx-font-weight: bold;");
+        GridPane.setMargin(createLoginButton, new Insets(20, 0,20,0));
+
+
+        createLoginButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                if(userField.getText().isEmpty()) {
+                    showAlert(Alert.AlertType.ERROR, gridPaneLogin.getScene().getWindow(), "Form Error!", "Please enter your username");
+                    return;
+                }
+                if(passwordField.getText().isEmpty()) {
+                    showAlert(Alert.AlertType.ERROR, gridPaneLogin.getScene().getWindow(), "Form Error!", "Please enter a password");
+                    return;
+                }
+
+                System.out.println("User " + userField.getText() + " has logged in");
+
+                //stage.setScene(ProfilePane.ProfilePane());
+
+            }
+
+        });
+
+
+        Button createAccountButton = new Button("Create Account");
+        createAccountButton.setPrefHeight(40);
+        createAccountButton.setDefaultButton(true);
+        createAccountButton.setPrefWidth(400);
+        createAccountButton.setTranslateX(410);
+        createAccountButton.setTranslateY(260);
+        createAccountButton.setStyle("-fx-background-color: #3D405B; -fx-text-fill: #F4F1DE; -fx-font-weight: bold;");
+        GridPane.setMargin(createAccountButton, new Insets(20, 0,20,0));
+
+        createAccountButton.setOnAction(new EventHandler<ActionEvent>(){
+            public void handle(ActionEvent t){
+                stage.setScene(CreateAccountScene());
+            }
+        });
+
+        BorderPane pane = new BorderPane();
+        pane.setLeft(root);
+        pane.setRight(gridPaneLogin);
+
+        gridPaneLogin.getChildren().addAll(createLoginButton, createAccountButton);
+        return new Scene(pane, 1200, 700);
+
+    }
+
+
+    private static Scene CreateAccountScene() {
+
+        HBox root = new HBox();
+
+        root.setStyle("-fx-background-color: white;");
+
+        String imageUrl = "https://images.unsplash.com/photo-1511690656952-34342bb7c2f2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80";
+
+        Image image = new Image(imageUrl,550,700,false,true);
+
+        ImageView imageView = new ImageView(image);
+
+        root.getChildren().add(imageView);
+
+
+        GridPane gridPaneRegister = new GridPane();
+
+        gridPaneRegister.setTranslateX(110);
+        gridPaneRegister.setTranslateY(200);
+
+        gridPaneRegister.setPadding(new Insets(40, 40, 40, 40));
+
+        gridPaneRegister.setHgap(10);
+        gridPaneRegister.setVgap(10);
+
+
+        Label headerLabel = new Label("User Registration");
+        headerLabel.setFont(Font.font("Arial", FontWeight.BOLD, 24));
+        gridPaneRegister.add(headerLabel, 0,0,2,1);
+        headerLabel.setTranslateX(100);
+        headerLabel.setTranslateY(-30);
+        GridPane.setMargin(headerLabel, new Insets(20, 0,20,0));
+
+
+        Label nameLabel = new Label("Full Name : ");
+        nameLabel.setTranslateX(-80);
+        nameLabel.setTranslateY(50);
+
+        TextField nameField = new TextField();
+        nameField.setPromptText("Full Name");
+        nameField.setFocusTraversable(false);
+        nameField.setPrefWidth(400);
+        nameField.setPrefHeight(40);
+        nameField.setTranslateY(50);
+
+
+        Label userLabel = new Label("Username :");
+        userLabel.setTranslateX(-80);
+        userLabel.setTranslateY(100);
+
+        TextField userField = new TextField();
+        userField.setPromptText("Username");
+        userField.setFocusTraversable(false);
+        userField.setPrefWidth(400);
+        userField.setPrefHeight(40);
+        userField.setTranslateY(100);
+
+
+        Label emailLabel = new Label("Email : ");
+        emailLabel.setTranslateX(-80);
+        emailLabel.setTranslateY(150);
+
+        TextField emailField = new TextField();
+        emailField.setPromptText("example@hotmail.co.uk");
+        emailField.setFocusTraversable(false);
+        emailField.setPrefWidth(400);
+        emailField.setPrefHeight(40);
+        emailField.setTranslateY(150);
+
+
+        Label passwordLabel = new Label("Password : ");
+        passwordLabel.setTranslateX(-80);
+        passwordLabel.setTranslateY(200);
+
+        PasswordField passwordField = new PasswordField();
+        passwordField.setPromptText("Password");
+        passwordField.setFocusTraversable(false);
+        passwordField.setPrefWidth(400);
+        passwordField.setPrefHeight(40);
+        passwordField.setTranslateY(200);
+
+
+        Button submitButton = new Button("Submit");
+        submitButton.setPrefHeight(40);
+        submitButton.setDefaultButton(true);
+        submitButton.setPrefWidth(400);
+        submitButton.setTranslateX(-40);
+        submitButton.setTranslateY(270);
+        submitButton.setStyle("-fx-background-color: #3D405B; -fx-text-fill: #F4F1DE; -fx-font-weight: bold; ");
+        GridPane.setHalignment(submitButton, HPos.CENTER);
+        GridPane.setMargin(submitButton, new Insets(20, 0,20,50));
+
+        submitButton.setOnAction(new EventHandler<ActionEvent>(){
+            public void handle(ActionEvent t){
+                if(nameField.getText().isEmpty()) {
+                    showAlert(Alert.AlertType.ERROR, gridPaneRegister.getScene().getWindow(), "Form Error!", "Please enter your name");
+                    return;
+                }
+                if(userField.getText().isEmpty()) {
+                    showAlert(Alert.AlertType.ERROR, gridPaneRegister.getScene().getWindow(), "Form Error!", "Please enter your username");
+                    return;
+                }
+                if(emailField.getText().isEmpty()) {
+                    showAlert(Alert.AlertType.ERROR, gridPaneRegister.getScene().getWindow(), "Form Error!", "Please enter your email id");
+                    return;
+                }
+                if(passwordField.getText().isEmpty()) {
+                    showAlert(Alert.AlertType.ERROR, gridPaneRegister.getScene().getWindow(), "Form Error!", "Please enter a password");
+                    return;
+                }
+
+                showAlert(Alert.AlertType.CONFIRMATION, gridPaneRegister.getScene().getWindow(), "Registration Successful!", "Welcome " + userField.getText());
+
+                System.out.println("Account for user " + userField.getText() + " was created successfully");
+            }
+        });
+
+        Button backButton = new Button("Back");
+        backButton.setPrefHeight(40);
+        backButton.setDefaultButton(true);
+        backButton.setPrefWidth(400);
+        backButton.setTranslateX(-40);
+        backButton.setTranslateY(320);
+        backButton.setStyle("-fx-background-color: #3D405B; -fx-text-fill: #F4F1DE; -fx-font-weight: bold; ");
+        GridPane.setHalignment(backButton, HPos.CENTER);
+        GridPane.setMargin(backButton, new Insets(20, 0,20,50));
+
+        backButton.setOnAction(new EventHandler<ActionEvent>(){
+            public void handle(ActionEvent t){
+                stage.setScene(logInScene());
+            }
+        });
+
+        BorderPane pane = new BorderPane();
+        pane.setLeft(gridPaneRegister);
+        pane.setRight(root);
+
+        gridPaneRegister.getChildren().addAll(nameLabel, nameField, userLabel, userField, emailLabel, emailField,
+                passwordLabel,passwordField, submitButton, backButton);
+        return new Scene(pane,1200, 700);
+
+    }
+
+
+
+    private static void showAlert(Alert.AlertType alertType, Window owner, String title, String message) {
+        Alert alert = new Alert(alertType);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.initOwner(owner);
+        alert.show();
+    }
+
+    public static void main(String[] args) {
+
+        launch(args);
+    }
+
+}
---
+++ Health Tracker 1/ProfilePane.java
@@ -0,0 +1,273 @@
+package Frontend;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.Node;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+
+
+public class ProfilePane extends BorderPane implements EventHandler<ActionEvent> {
+    // photo
+    ImageView ivPhoto;
+    File photoFile;
+
+    // general
+    TextField tfUsername;
+    TextField tfRealName;
+    TextField tfEmail;
+
+    // personal information
+    TextField tfHeight;
+    TextField tfWeight;
+    TextField tfBMI;
+    TextField tfTargetWeight;
+    TextField tfHealthOverview;
+    Button btnGenerate;
+
+    // exercise
+    TextField tfDuration;
+    TextField tfDistance;
+    ComboBox<String> cbTypeOfSports;
+
+    // diet
+    ComboBox<String> cbFood;
+    ComboBox<String> cbDrink;
+    ComboBox<String> cbMealType;
+    TextField tfCalorieCount;
+
+    // save button
+    Button btnSave;
+
+    public ProfilePane() {
+
+        VBox rootBox = new VBox(10);
+        rootBox.setPadding(new Insets(10));
+
+        rootBox.getChildren().add(createPhotoPart());
+        rootBox.getChildren().add(createGeneralPart());
+        rootBox.getChildren().add(createPersonalPart());
+        rootBox.getChildren().add(createExercisePart());
+        rootBox.getChildren().add(createDietPart());
+
+        rootBox.getChildren().add(createSaveButtonPart());
+
+        ScrollPane scrollPane = new ScrollPane(rootBox);
+        setCenter(scrollPane);
+    }
+
+    public String getTitle() {
+        return "Profile";
+    }
+
+    @Override
+    public void handle(ActionEvent event) {
+        Alert alert = new Alert(AlertType.INFORMATION);
+        alert.setContentText("Information Saved");
+        alert.showAndWait();
+    }
+
+    private Node createDietPart() {
+        cbFood = new ComboBox<String>();
+        cbFood.setEditable(true);
+        cbFood.getItems().addAll("Hamburger", "Salad", "Ice Cream");
+
+        cbDrink = new ComboBox<String>();
+        cbDrink.setEditable(true);
+        cbDrink.getItems().addAll("Water", "Cola", "Sprite");
+
+        cbMealType = new ComboBox<String>();
+        cbMealType.setEditable(true);
+        cbMealType.getItems().addAll("Vegan", "Fruit", "Starch");
+
+        tfCalorieCount = new TextField();
+
+        GridPane gridPane = createGridPane();
+        gridPane.add(createLabel("Food"), 0, 0);
+        gridPane.add(createLabel("Drink"), 0, 1);
+        gridPane.add(createLabel("Meal Type"), 0, 2);
+        gridPane.add(createLabel("Calorie Count"), 0, 3);
+
+        gridPane.add(cbFood, 1, 0);
+        gridPane.add(cbDrink, 1, 1);
+        gridPane.add(cbMealType, 1, 2);
+        gridPane.add(tfCalorieCount, 1, 3);
+
+        return new VBox(createTitleLabel("Diet"), gridPane);
+    }
+
+    private Node createExercisePart() {
+        tfDuration = new TextField();
+        tfDistance = new TextField();
+        cbTypeOfSports = new ComboBox<String>();
+        cbTypeOfSports.setEditable(true);
+        cbTypeOfSports.getItems().addAll("Swimming", "Togging", "Yoga");
+
+        GridPane gridPane = createGridPane();
+        gridPane.add(createLabel("Duration"), 0, 0);
+        gridPane.add(createLabel("Distance"), 0, 1);
+        gridPane.add(createLabel("Type of Sports"), 0, 2);
+        gridPane.add(tfDuration, 1, 0);
+        gridPane.add(tfDistance, 1, 1);
+        gridPane.add(cbTypeOfSports, 1, 2);
+
+        return new VBox(createTitleLabel("Exercise"), gridPane);
+    }
+
+    private Node createPersonalPart() {
+        tfHeight = new TextField();
+        tfWeight = new TextField();
+        tfBMI = new TextField();
+        tfTargetWeight = new TextField();
+        tfHealthOverview = new TextField();
+        btnGenerate = new Button("Generate");
+
+        btnGenerate.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent arg0) {
+                double weight, height;
+                try {
+                    height = Double.parseDouble(tfHeight.getText());
+                } catch (NumberFormatException e) {
+                    showMsg("Invalid Height");
+                    return;
+                }
+
+                try {
+                    weight = Double.parseDouble(tfWeight.getText());
+                } catch (NumberFormatException e) {
+                    showMsg("Invalid Weight");
+                    return;
+                }
+
+                double bmi = weight / (height * height);
+                tfBMI.setText(String.format("%.2f", bmi));
+            }
+        });
+
+        GridPane gridPane = createGridPane();
+        gridPane.add(createLabel("Height(m)"), 0, 0);
+        gridPane.add(createLabel("Weight(kg)"), 0, 1);
+        gridPane.add(createLabel("Current BMI"), 0, 2);
+        gridPane.add(createLabel("Target Weight"), 0, 3);
+        gridPane.add(createLabel("Health Overview"), 0, 4);
+
+        gridPane.add(tfHeight, 1, 0);
+        gridPane.add(tfWeight, 1, 1);
+        gridPane.add(tfBMI, 1, 2);
+        gridPane.add(tfTargetWeight, 1, 3);
+        gridPane.add(tfHealthOverview, 1, 4);
+        gridPane.add(btnGenerate, 3, 2);
+
+        return new VBox(createTitleLabel("Personal Information"), gridPane);
+    }
+
+    private VBox createSaveButtonPart() {
+        btnSave = new Button("  Save  ");
+        btnSave.setOnAction(this);
+
+        VBox vBox = new VBox(btnSave);
+        vBox.setPadding(new Insets(10, 10, 10, 150));
+        return vBox;
+    }
+
+    private VBox createPhotoPart() {
+        ivPhoto = new ImageView();
+        Image image = new Image(getClass().getResourceAsStream("photo_default.jpg"));
+        ivPhoto.fitWidthProperty().set(150);
+        ivPhoto.fitHeightProperty().set(150);
+        ivPhoto.setImage(image);
+        VBox vBox = new VBox(ivPhoto);
+        vBox.setPadding(new Insets(10, 10, 10, 150));
+
+        ivPhoto.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+
+                JFileChooser chooser = new JFileChooser(new File("."));
+                chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+                chooser.setFileFilter(
+                        new FileNameExtensionFilter("image files", "jpg", "png", "bmp"));
+
+                int status = chooser.showOpenDialog(null);
+                if (status == JFileChooser.APPROVE_OPTION) {
+                    File selectedFile = chooser.getSelectedFile();
+                    if (selectedFile != null) {
+                        try {
+                            Image image = new Image(new FileInputStream(selectedFile));
+                            ivPhoto.setImage(image);
+
+                            photoFile = selectedFile;
+                        } catch (FileNotFoundException e) {
+                            showMsg("failed to read image");
+                        }
+                    }
+                }
+            }
+        });
+        return vBox;
+    }
+
+    private VBox createGeneralPart() {
+        tfUsername = new TextField();
+        tfRealName = new TextField();
+        tfEmail = new TextField();
+
+        GridPane gridPane = createGridPane();
+        gridPane.add(createLabel("Username"), 0, 0);
+        gridPane.add(createLabel("Real Name"), 0, 1);
+        gridPane.add(createLabel("Email Address"), 0, 2);
+        gridPane.add(tfUsername, 1, 0);
+        gridPane.add(tfRealName, 1, 1);
+        gridPane.add(tfEmail, 1, 2);
+
+        return new VBox(createTitleLabel("General"), gridPane);
+    }
+
+    private GridPane createGridPane() {
+        GridPane gridPane = new GridPane();
+        gridPane.setPadding(new Insets(5, 0, 0, 50));
+        gridPane.setHgap(10);
+        gridPane.setVgap(5);
+        return gridPane;
+    }
+
+    private Label createLabel(String text) {
+        Label label = new Label(text);
+        label.setFont(new Font(20));
+        return label;
+    }
+
+    private Label createTitleLabel(String text) {
+        Label label = new Label(text);
+        label.setFont(new Font(25));
+        return label;
+    }
+
+    private void showMsg(String text) {
+        Alert alert = new Alert(AlertType.WARNING);
+        alert.setContentText(text);
+        alert.showAndWait();
+    }
+
+}
