79b7942899ce3895baeaf3253558444000ab7e7a
Dawn296378 <80273834+Dawn296378@users.noreply.github.com>
2021-05-04 21:51:38 +0100

    ProfileGoal2
    make page in center

---
+++ ProfileGoal2/FrontEndDemo.java
@@ -0,0 +1,20 @@
+import frontend.ProfilePane;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class FrontEndDemo extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        ProfilePane root = new ProfilePane();
+ 
+        stage.setScene(new Scene(root));
+        stage.setTitle(root.getTitle());
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
