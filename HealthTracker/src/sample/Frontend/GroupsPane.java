35251ed0b41362c04df2b20d8b29659925a36693
Thiruisa <45204969+Thiruisa@users.noreply.github.com>
2021-05-10 15:33:26 +0100

    Add files via upload

--- HealthTracker/src/sample/Frontend/GroupsPane.java
+++ HealthTracker/src/sample/Frontend/GroupsPane.java
@@ -1,218 +1,266 @@
-package Frontend;
+package client;
 
+import com.jfoenix.controls.JFXHamburger;
+import com.jfoenix.transitions.hamburger.HamburgerNextArrowBasicTransition;
+import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.HPos;
 import javafx.geometry.Insets;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.Font;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.stage.Stage;
+import com.jfoenix.controls.JFXButton;
+import javafx.scene.control.Label;
+import javafx.scene.layout.*;
 
 
-public class GroupsPane extends BorderPane implements EventHandler<ActionEvent> {
 
-    Label headerLabel;
+public class GroupsPane extends Application {
+    private static Stage stage;
+    @Override
+    public void start(Stage primaryStage) throws Exception {
 
-    // Existing Group Button
-    Button btnGroupOne;
-    Button btnGroupTwo;
-    Button btnGroupThree;
-    Button btnGroupFour;
-
-    // Create Group
-    TextField nameField;
-    ComboBox<String> cbGoal;
-    TextField friendField;
-    Button btnAddFriend;
-    Button btnCreateGroup;
-
-    // Join Group
-    ComboBox<String> cbJoin;
-    Button btnJoinGroup;
-
-
-    public GroupsPane() {
-
-        VBox rootBox = new VBox(10);
-        rootBox.setPadding(new Insets(10));
-
-        rootBox.getChildren().add(title());
-        rootBox.getChildren().addAll(existingGroup());
-        rootBox.getChildren().addAll(createGroup());
-        rootBox.getChildren().addAll(joinGroup());
-
-        ScrollPane scrollPane = new ScrollPane(rootBox);
-        setCenter(scrollPane);
-
-    }
-
-    public String getTitle() {
-        return "Groups";
+        primaryStage.setTitle("Groups");
+        stage = primaryStage;
+        Scene scene = groupScene();
+        primaryStage.setScene(scene);
+        primaryStage.sizeToScene();
+        primaryStage.show();
     }
 
 
-    public void handle(ActionEvent event) {
-        Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        //alert.setContentText("Information Saved");
-        alert.showAndWait();
-    }
+    public static Scene groupScene() throws Exception {
 
-    public Node title(){
-        headerLabel = new Label("Groups");
+        VBox vBox = new VBox(50);
 
-        GridPane gridPane = createGridPane();
-        gridPane.add(headerLabel, 1, 0);
+        vBox.setStyle("-fx-background-color: #3D405B;");
 
-        VBox vBox = new VBox(headerLabel);
-        vBox.setPadding(new Insets(20, 0, 0, 400));
+        vBox.setPrefWidth(230);
 
-        return new VBox(gridPane);
-    }
+        JFXButton profile = new JFXButton("Profile");
+        profile.setStyle("-fx-text-fill: #F4F1DE; -fx-font-weight: bold; -fx-font-size: 20; -fx-alignment: center");
+        profile.setPrefSize(230, 100);
 
-    public Node existingGroup(){
-        btnGroupOne = new Button("Group 1");
-        btnGroupTwo = new Button("Group 2");
-        btnGroupThree = new Button("Group 3");
-        btnGroupFour = new Button("Group 4");
 
-        GridPane gridPane = createGridPane();
-        gridPane.add(btnGroupOne, 1, 6);
-        gridPane.add(btnGroupTwo, 2, 6);
-        gridPane.add(btnGroupThree, 3, 6);
-        gridPane.add(btnGroupFour, 4, 6);
 
-        btnGroupOne.setPrefSize(100, 60);
-        btnGroupTwo.setPrefSize(100, 60);
-        btnGroupThree.setPrefSize(100, 60);
-        btnGroupFour.setPrefSize(100, 60);
+        JFXButton goals = new JFXButton("Goals");
+        goals.setStyle("-fx-text-fill: #F4F1DE; -fx-font-weight: bold; -fx-font-size: 20; -fx-alignment: center");
+        goals.setPrefWidth(230);
 
-        btnGroupOne.setStyle("-fx-background-color: #EAB69F; -fx-text-fill: #F4F1DE; -fx-font-weight: bold;");
-        btnGroupTwo.setStyle("-fx-background-color: #E07A5F; -fx-text-fill: #F4F1DE; -fx-font-weight: bold;");
-        btnGroupThree.setStyle("-fx-background-color: #81B29A; -fx-text-fill: #F4F1DE; -fx-font-weight: bold;");
-        btnGroupFour.setStyle("-fx-background-color: #F2CC8F; -fx-text-fill: #F4F1DE; -fx-font-weight: bold;");
+        JFXButton summary = new JFXButton("Weekly Summary");
+        summary.setStyle("-fx-text-fill: #F4F1DE; -fx-font-weight: bold; -fx-font-size: 20; -fx-alignment: center");
+        summary.setPrefWidth(230);
 
-        return new VBox(gridPane);
-    }
+        JFXButton groups = new JFXButton("Groups");
+        groups.setStyle("-fx-text-fill: #F4F1DE; -fx-font-weight: bold; -fx-font-size: 20; -fx-alignment: center");
+        groups.setPrefWidth(230);
 
-    public Node createGroup(){
-
-        nameField = new TextField();
-
-        cbGoal = new ComboBox<String>();
-        cbGoal.setEditable(true);
-        cbGoal.getItems().addAll("Exercise", "Calories", "Custom");
-
-        friendField = new TextField();
-
-        btnAddFriend = new Button("Add Friend");
-
-        btnAddFriend.setPrefSize(100, 40);
-        btnAddFriend.setStyle("-fx-background-color: #3D405B; -fx-text-fill: #F4F1DE; -fx-font-weight: bold;");
-        //btnAddFriend.setPadding(new Insets(5, 0, 0, 40));
-
-        btnCreateGroup = new Button("Create Group");
-
-        btnCreateGroup.setPrefSize(300, 40);
-        btnCreateGroup.setStyle("-fx-background-color: #3D405B; -fx-text-fill: #F4F1DE; -fx-font-weight: bold;");
-
-        btnCreateGroup.setOnAction(new EventHandler<ActionEvent>(){
-            public void handle(ActionEvent t){
-                if(nameField.getText().isEmpty()) {
-                    showAlert(Alert.AlertType.ERROR, "Error!", "Please enter a unique group name");
-                    return;
+        groups.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent event) {
+                try {
+                    stage.setScene(GroupsPane.groupScene());
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
-                if(cbGoal.getSelectionModel().isEmpty()) {
-                    showAlert(Alert.AlertType.ERROR, "Error!", "Please enter your username");
-                    return;
+            }
+        });
+
+        JFXButton logout = new JFXButton("Logout");
+        logout.setStyle("-fx-text-fill: #F4F1DE; -fx-font-weight: bold; -fx-font-size: 20; -fx-alignment: center");
+        logout.setPrefWidth(230);
+        logout.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent event) {
+                try {
+                    stage.setScene(Main.logInScene());
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
+            }
+        });
 
-                showAlert(Alert.AlertType.CONFIRMATION, "Created Group Successful!", "Group " + nameField.getText());
+        vBox.setAlignment(Pos.BASELINE_LEFT);
 
-                System.out.println("Created group " + nameField.getText() + " successfully");
+        vBox.getChildren().addAll(profile, goals, summary, groups, logout);
+        vBox.setPadding(new Insets(0, 0, 0, 0));
+
+
+        GridPane gridPaneGroup = new GridPane();
+
+        Label headerLabel = new Label("Groups");
+        headerLabel.setFont(Font.font("Arial", FontWeight.BOLD, 40));
+        gridPaneGroup.add(headerLabel, 0, 0, 2, 1);
+        headerLabel.setAlignment(Pos.CENTER);
+        headerLabel.setTranslateX(-200);
+        headerLabel.setTranslateY(0);
+        GridPane.setHalignment(headerLabel, HPos.CENTER);
+        GridPane.setMargin(headerLabel, new Insets(20, 0, 20, 0));
+
+
+        Label secondLabel = new Label("Create Group");
+        secondLabel.setFont(Font.font("Arial", FontWeight.BOLD, 30));
+        gridPaneGroup.add(secondLabel, 0, 0, 2, 1);
+        secondLabel.setAlignment(Pos.CENTER);
+        secondLabel.setTranslateX(-430);
+        secondLabel.setTranslateY(70);
+        GridPane.setHalignment(secondLabel, HPos.CENTER);
+        GridPane.setMargin(secondLabel, new Insets(20, 0, 20, 0));
+
+        Label groupLabel = new Label("Group Name");
+        groupLabel.setFont(Font.font("Arial", FontWeight.BOLD, 15));
+        gridPaneGroup.add(groupLabel, 0, 0, 2, 1);
+        groupLabel.setAlignment(Pos.CENTER);
+        groupLabel.setTranslateX(-600);
+        groupLabel.setTranslateY(150);
+        GridPane.setHalignment(groupLabel, HPos.CENTER);
+        GridPane.setMargin(groupLabel, new Insets(20, 0, 20, 0));
+
+        TextField nameField = new TextField();
+        nameField.setPrefHeight(40);
+        nameField.setMaxWidth(400);
+        nameField.setTranslateX(-530);
+        nameField.setTranslateY(90);
+        gridPaneGroup.add(nameField, 1,2);
+
+
+        Label goalLabel = new Label("Group Goal");
+        goalLabel.setFont(Font.font("Arial", FontWeight.BOLD, 15));
+        gridPaneGroup.add(goalLabel, 0, 0, 2, 1);
+        goalLabel.setAlignment(Pos.CENTER);
+        goalLabel.setTranslateX(-600);
+        goalLabel.setTranslateY(210);
+        GridPane.setHalignment(goalLabel, HPos.CENTER);
+        GridPane.setMargin(goalLabel, new Insets(20, 0, 20, 0));
+
+        ComboBox cbGoal = new ComboBox();
+        cbGoal.getItems().addAll(
+                "Exercise", "Calories", "Custom"
+        );
+        cbGoal.setPrefHeight(40);
+        cbGoal.setPrefWidth(300);
+        cbGoal.setTranslateX(-530);
+        cbGoal.setTranslateY(150);
+        gridPaneGroup.add(cbGoal, 1,2);
+
+        Label addFriend = new Label("Add Friend");
+        addFriend.setFont(Font.font("Arial", FontWeight.BOLD, 15));
+        gridPaneGroup.add(addFriend, 0, 0, 2, 1);
+        addFriend.setAlignment(Pos.CENTER);
+        addFriend.setTranslateX(-600);
+        addFriend.setTranslateY(270);
+        GridPane.setHalignment(addFriend, HPos.CENTER);
+        GridPane.setMargin(addFriend, new Insets(20, 0, 20, 0));
+
+        TextField friendField = new TextField();
+        friendField.setPrefHeight(40);
+        friendField.setPrefWidth(300);
+        friendField.setTranslateX(-530);
+        friendField.setTranslateY(170);
+        gridPaneGroup.add(friendField, 1,4);
+
+        Button addFriendButton = new Button("Add Friend");
+        addFriendButton.setPrefHeight(40);
+        addFriendButton.setDefaultButton(true);
+        addFriendButton.setPrefWidth(100);
+        addFriendButton.setTranslateX(-130);
+        addFriendButton.setTranslateY(330);
+        addFriendButton.setStyle("-fx-background-color: #3D405B; -fx-text-fill: #F4F1DE; -fx-font-weight: bold;");
+        GridPane.setMargin(addFriendButton, new Insets(20, 0, 20, 0));
+
+        addFriendButton.setOnAction(t -> {
+            if(cbGoal.getSelectionModel().isEmpty()) {
+                showAlert(Alert.AlertType.ERROR, "Error!", "Please add a friend");
             }
         });
 
 
-        GridPane gridPane = createGridPane();
-        gridPane.add(createLabel("Group Name"), 0, 0);
-        gridPane.add(createLabel("Group Goal"), 0, 1);
-        gridPane.add(createLabel("Add Friend"), 0, 2);
+        Button createGroupButton = new Button("Create New Group");
+        createGroupButton.setPrefHeight(40);
+        createGroupButton.setDefaultButton(true);
+        createGroupButton.setPrefWidth(300);
+        createGroupButton.setTranslateX(-230);
+        createGroupButton.setTranslateY(400);
+        createGroupButton.setStyle("-fx-background-color: #3D405B; -fx-text-fill: #F4F1DE; -fx-font-weight: bold;");
+        GridPane.setMargin(createGroupButton, new Insets(20, 0, 20, 0));
 
-        gridPane.add(nameField, 1, 0);
-        gridPane.add(cbGoal, 1, 1);
-        gridPane.add(friendField, 1, 2);
-        gridPane.add(btnAddFriend, 1, 3);
-        gridPane.add(btnCreateGroup, 1, 4);
-
-        return new VBox(createTitleLabel("Create Group"), gridPane);
-
-    }
-
-    private Node joinGroup(){
-
-        cbJoin = new ComboBox<String>();
-        cbJoin.setEditable(true);
-        cbJoin.getItems().addAll("Group 10", "Group 11", "Group 12", "Group 13", "Group 14");
-
-        btnJoinGroup = new Button("Join Group");
-
-        btnJoinGroup.setPrefSize(300, 40);
-        btnJoinGroup.setStyle("-fx-background-color: #3D405B; -fx-text-fill: #F4F1DE; -fx-font-weight: bold;");
-
-        btnJoinGroup.setOnAction(new EventHandler<ActionEvent>(){
-            public void handle(ActionEvent t){
-
-                if(cbJoin.getSelectionModel().isEmpty()) {
-                    showAlert(Alert.AlertType.ERROR, "Error!", "Please select a group to join");
-                    return;
-                }
-
-                showAlert(Alert.AlertType.CONFIRMATION, "Created Group Successful!", "Group " + nameField.getText());
-
-                System.out.println("Joined group " + cbJoin.getSelectionModel() + " successfully");
+        createGroupButton.setOnAction(t -> {
+            if(nameField.getText().isEmpty()) {
+                showAlert(Alert.AlertType.ERROR, "Form Error!", "Please enter a unique group name");
+                return;
             }
+            if(cbGoal.getSelectionModel().isEmpty()) {
+                showAlert(Alert.AlertType.ERROR, "Error!", "Please select a goal for the group");
+                return;
+            }
+
+            showAlert(Alert.AlertType.CONFIRMATION, "Created Group Successful!", "Group " + nameField.getText());
+
+            System.out.println("Created group " + nameField.getText() + " successfully");
         });
 
-        GridPane gridPane = createGridPane();
-        gridPane.add(createLabel("Group Name"), 0, 0);
 
 
-        gridPane.add(cbJoin, 1, 0);
-        gridPane.add(btnJoinGroup, 1, 1);
+        Label thirdLabel = new Label("Join Group");
+        thirdLabel.setFont(Font.font("Arial", FontWeight.BOLD, 30));
+        gridPaneGroup.add(thirdLabel, 0, 0, 2, 1);
+        thirdLabel.setAlignment(Pos.CENTER);
+        thirdLabel.setTranslateX(30);
+        thirdLabel.setTranslateY(70);
+        GridPane.setHalignment(thirdLabel, HPos.CENTER);
+        GridPane.setMargin(thirdLabel, new Insets(20, 0, 20, 0));
 
-        return new VBox(createTitleLabel("Join Group"), gridPane);
+        Label goalLabel_one = new Label("Group Name");
+        goalLabel_one.setFont(Font.font("Arial", FontWeight.BOLD, 15));
+        gridPaneGroup.add(goalLabel_one, 0, 0, 2, 1);
+        goalLabel_one.setAlignment(Pos.CENTER);
+        goalLabel_one.setTranslateX(-120);
+        goalLabel_one.setTranslateY(150);
+        GridPane.setHalignment(goalLabel_one, HPos.CENTER);
+        GridPane.setMargin(goalLabel_one, new Insets(20, 0, 20, 0));
+
+        ComboBox cbJoin = new ComboBox();
+        cbJoin.getItems().addAll(
+                "Group 10", "Group 11", "Group 12", "Group 13", "Group 14"
+        );
+        cbJoin.setPrefHeight(40);
+        cbJoin.setPrefWidth(300);
+        cbJoin.setTranslateX(-50);
+        cbJoin.setTranslateY(90);
+        gridPaneGroup.add(cbJoin, 1,2);
+
+        Button joinGroupButton = new Button("Join a Group");
+        joinGroupButton.setPrefHeight(40);
+        joinGroupButton.setDefaultButton(true);
+        joinGroupButton.setPrefWidth(300);
+        joinGroupButton.setTranslateX(230);
+        joinGroupButton.setTranslateY(400);
+        joinGroupButton.setStyle("-fx-background-color: #3D405B; -fx-text-fill: #F4F1DE; -fx-font-weight: bold;");
+        GridPane.setMargin(joinGroupButton, new Insets(20, 0, 20, 0));
+
+        joinGroupButton.setOnAction(t -> {
+
+            if(cbJoin.getSelectionModel().isEmpty()) {
+                showAlert(Alert.AlertType.ERROR, "Error!", "Please select a group to join");
+                return;
+            }
+
+        });
+
+        BorderPane menu = new BorderPane();
+        menu.setLeft(vBox);
+        menu.setRight(gridPaneGroup);
+
+        gridPaneGroup.getChildren().addAll(addFriendButton, createGroupButton, joinGroupButton);
+
+        return new Scene(menu, 1200, 700);
     }
 
-
-    private GridPane createGridPane() {
-        GridPane gridPane = new GridPane();
-        gridPane.setPadding(new Insets(5, 0, 0, 400));
-        gridPane.setHgap(10);
-        gridPane.setVgap(5);
-        return gridPane;
-    }
-
-    private Label createLabel(String text) {
-        Label label = new Label(text);
-        label.setFont(new Font(20));
-        return label;
-    }
-
-    private Label createTitleLabel(String text) {
-        Label label = new Label(text);
-        label.setFont(new Font(25));
-        label.setPadding(new Insets(5, 0, 0, 400));
-        return label;
-    }
-
-    private void showMsg(String text) {
-        Alert alert = new Alert(Alert.AlertType.WARNING);
-        alert.setContentText(text);
-        alert.showAndWait();
-    }
 
     private static void showAlert(Alert.AlertType alertType, String title, String message) {
         Alert alert = new Alert(alertType);
@@ -222,5 +270,9 @@ public class GroupsPane extends BorderPane implements EventHandler<ActionEvent> 
         alert.show();
     }
 
+    public static void main(String[] args) {
+
+        launch(args);
+    }
 
 }
