bbaae4ed0498e21fdcba69fe3800c0e4c81818cc
Thiruisa <45204969+Thiruisa@users.noreply.github.com>
2021-05-11 11:59:50 +0100

    Add files via upload

---
+++ Health Tracker Final/src/client/SideMenu.java
@@ -0,0 +1,126 @@
+package client;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+
+public class SideMenu extends Application {
+
+    private static Stage stage;
+
+    @Override
+    public void start(Stage primaryStage) {
+
+        primaryStage.setTitle("Groups");
+        stage = primaryStage;
+        Scene scene = sideMenuScene();
+        primaryStage.setScene(scene);
+        primaryStage.sizeToScene();
+        primaryStage.show();
+    }
+
+
+    public static Scene sideMenuScene() {
+
+
+
+
+    VBox vBox = new VBox(50);
+
+        vBox.setStyle("-fx-background-color: #3D405B;");
+
+        vBox.setPrefWidth(230);
+
+    JFXButton profile = new JFXButton("Profile");
+        profile.setStyle("-fx-text-fill: #F4F1DE; -fx-font-weight: bold; -fx-font-size: 20; -fx-alignment: center");
+        profile.setPrefSize(230, 100);
+
+        profile.setOnAction(new EventHandler<ActionEvent>() {
+        @Override
+        public void handle(ActionEvent event) {
+            try {
+                stage.setScene(ProfilePane.profileScene());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    });
+
+    JFXButton goals = new JFXButton("Goals");
+        goals.setStyle("-fx-text-fill: #F4F1DE; -fx-font-weight: bold; -fx-font-size: 20; -fx-alignment: center");
+        goals.setPrefWidth(230);
+
+        goals.setOnAction(new EventHandler<ActionEvent>() {
+        @Override
+        public void handle(ActionEvent event) {
+            try {
+                stage.setScene(GoalSettingPane.goalSettingScene());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    });
+
+    JFXButton summary = new JFXButton("Weekly Summary");
+        summary.setStyle("-fx-text-fill: #F4F1DE; -fx-font-weight: bold; -fx-font-size: 20; -fx-alignment: center");
+        summary.setPrefWidth(230);
+
+        summary.setOnAction(new EventHandler<ActionEvent>() {
+        @Override
+        public void handle(ActionEvent event) {
+            try {
+                stage.setScene(WeeklySummary.summaryScene());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    });
+
+    JFXButton groups = new JFXButton("Groups");
+        groups.setStyle("-fx-text-fill: #F4F1DE; -fx-font-weight: bold; -fx-font-size: 20; -fx-alignment: center");
+        groups.setPrefWidth(230);
+
+        groups.setOnAction(new EventHandler<ActionEvent>() {
+        @Override
+        public void handle(ActionEvent event) {
+            try {
+                stage.setScene(GroupsPane.groupScene());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    });
+
+    JFXButton logout = new JFXButton("Logout");
+        logout.setStyle("-fx-text-fill: #F4F1DE; -fx-font-weight: bold; -fx-font-size: 20; -fx-alignment: center");
+        logout.setPrefWidth(230);
+
+        logout.setOnAction(new EventHandler<ActionEvent>() {
+        @Override
+        public void handle(ActionEvent event) {
+            try {
+                stage.setScene(Main.logInScene());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    });
+
+        vBox.setAlignment(Pos.BASELINE_LEFT);
+
+        vBox.getChildren().addAll(profile, goals, summary, groups, logout);
+
+        return new Scene(vBox, 1200, 700);
+}
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
